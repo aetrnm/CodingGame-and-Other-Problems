7134da587d9368285da3676c13b80a159bc693cd
Oleg Gnat <oleggnatkovskii@gmail.com>
2021-12-22 21:08:28 +0200

    2 tests added

--- SortingAlgorithms/tests_QuickSort.py
+++ SortingAlgorithms/tests_QuickSort.py
@@ -16,6 +16,12 @@ class QuickSortTestCases(unittest.TestCase):
     def test_zeros(self):
         self.assertEqual(SA.quickSort([0]), [0])
 
+    def test_allValuesDifferent(self):
+        self.assertEqual(SA.quickSort([7, 3, 4, 1, 2, 5, -1]), [-1, 1, 2, 3, 4, 5, 7])
+
+    def test_elementEqualToBarrier(self):
+        self.assertEqual(SA.quickSort([3, 1, 2, 4, 16, 9, 2, 10, 9, 23]), [1, 2, 2, 3, 4, 9, 9, 10, 16, 23])
+
 
 if __name__ == '__main__':
     unittest.main()
