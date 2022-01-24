3be68443ecd52e98e3bfb37653a4cd2efbdb89ed
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-01-24 23:38:07 +0200

    minor code improvements, seems to work

--- FibonacciSequence/main.py
+++ FibonacciSequence/main.py
@@ -1,23 +1,40 @@
 import decimal
+import math
 
-def binpow (a, n=1):
-	res = 1
-	while n:
-		if (n & 1): res *= a
-		a *= a
-		n >>= 1
-	return res
+MODULO = 1234567890
+
+
+def binpow(a, n=1):
+    res = 1
+    while n != 0:
+        print('RES: %d N: %d A: %d ' % (res, n, a))
+        if n % 2 != 0:
+            res *= a
+            res %= MODULO
+        a *= a
+        a %= MODULO
+        n //= 2
+    return res
+
 
 def getFibonacciNumber(n):
     decimal.getcontext().prec = 100
+    k = 76
 
-    root_5 = decimal.Decimal(5).sqrt()
+    root_5 = math.sqrt(5)
     phi = (1 + root_5) / 2
 
-    a = binpow(phi, n) / root_5
+    phiPowK = phi ** k
+    print("phipowK:", phiPowK)
+    phiPowKX = binpow(phiPowK, n // k)
+    print("phipowKX:", phiPowKX)
+    phiPowR = phi ** int(n % k)
+    print(phiPowR)
+    print("phipowR:", phiPowR)
+
+    a = phiPowKX * (phiPowR / root_5) % MODULO
 
     return a
 
-    
 
-print(getFibonacciNumber(1000000))
+print(getFibonacciNumber(10))
